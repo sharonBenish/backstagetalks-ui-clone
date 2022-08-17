2e3a283bd33adf1d64660cb80fb8216c81571ef1
MAC <sbenishwilliams@gmail.com>
2022-08-17 20:43:23 +0100

    added scroll snapping

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ const issue3 = document.getElementById('issue3');
 const issue4 = document.getElementById('issue4');
 const issue5 = document.getElementById('issue5');
 const issue6 = document.getElementById('issue6');
-const posters = Array.from (document.querySelectorAll('.poster'));
-window.addEventListener('scroll', ()=>{
-    const scrollPosition = document.documentElement.scrollTop;
+const main = document.querySelector('.main');
+main.addEventListener('scroll', ()=>{
+    const scrollPosition = main.scrollTop;
    if (scrollPosition >= issue1.offsetTop){
     document.body.className = "issue1"
    }
-   else if (scrollPosition > issue2.offsetTop){
+   else if (scrollPosition >= issue2.offsetTop){
     document.body.className = "issue2"
    }
-   else if (scrollPosition >=issue3.offsetTop){
+   else if (scrollPosition >= issue3.offsetTop){
     document.body.className = "issue3"
    }
-   else if (scrollPosition > issue4.offsetTop){
+   else if (scrollPosition >= issue4.offsetTop){
     document.body.className = "issue4"
    }
-   else if (scrollPosition > issue5.offsetTop){
+   else if (scrollPosition >= issue5.offsetTop){
     document.body.className = "issue5"
    }
-   else if (scrollPosition > issue6.offsetTop){
+   else if (scrollPosition >= issue6.offsetTop){
     document.body.className = "issue6"
    }
 })
-console.log(posters)
